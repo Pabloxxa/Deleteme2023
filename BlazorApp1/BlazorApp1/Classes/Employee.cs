9e10df17a17e079376c78701c3b00de281fb24c0
Juan Pablo Ossa Ríos <paoxxa@gmail.com>
2023-04-18 15:00:09 -0500

    Creating employee page

---
+++ BlazorApp1/BlazorApp1/Classes/Employee.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BlazorApp1.Classes
+{
+    public class Employee
+    {
+        public int Int { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public Date Birth { get; set; }
+        public Decimal Salary { get; set; }
+
+    }
+}
